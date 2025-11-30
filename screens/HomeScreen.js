528d9081e4b31e53722ff84010eb25ce6e0ec3eb
Amir <seyedamirhossein.sharabiani@studytafensw.edu.au>
2025-11-30 19:43:30 +1100

    Feat: Add screens

---
+++ screens/HomeScreen.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from 'react';
+import {Avatar, Card, IconButton, FAB, Snackbar, TextInput, Dialog, Portal, Button, Text, Surface, Divider, Searchbar, useTheme } from "react-native-paper";
+import {View, Image, TouchableOpacity, StyleSheet, ScrollView, ActivityIndicator} from "react-native";
+// import { TouchableOpacity } from "react-native-gesture-handler";
+import { useIsFocused } from "@react-navigation/native";
+import { Dropdown } from "react-native-paper-dropdown";
+
+export default function HomeScreen(props) {
+  return (
+    <Surface style={{flex:1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text  variant='displaySmall'>HomeScreen</Text>
+    </Surface>
+  )
+}
